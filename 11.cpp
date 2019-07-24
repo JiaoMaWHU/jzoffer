eba558f154606df21693b01c1c31fec7989c3f53
Jinxin Ma <jiaomajiaoma@hotmail.com>
2019-07-24 10:43:13 +0800

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -3,7 +3,7 @@ public:
     int minNumberInRotateArray(vector<int> rotateArray) {
         int x=0, y=rotateArray.size()-1, m;
         while(x<=y){
-            m=x+((y-x)>>2);
+            m=x+((y-x)>>1);
             if(rotateArray[m]>rotateArray[y]){
                 x=m+1;
             }else if(rotateArray[m]<rotateArray[x]){
